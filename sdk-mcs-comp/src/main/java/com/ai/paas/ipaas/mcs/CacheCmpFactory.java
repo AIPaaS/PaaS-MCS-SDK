409104783372695fd1d17aa6cadb77423487ba7e
douxiaofeng <douxf@asiainfo.com>
2016-12-08 13:50:36 +0800

    fix bug

--- sdk-mcs-comp/src/main/java/com/ai/paas/ipaas/mcs/CacheCmpFactory.java
+++ sdk-mcs-comp/src/main/java/com/ai/paas/ipaas/mcs/CacheCmpFactory.java
@@ -13,41 +13,44 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class CacheCmpFactory {
 
-    private static Map<String, ICacheClient> cacheClients = new ConcurrentHashMap<String, ICacheClient>();
+	private static Map<String, ICacheClient> cacheClients = new ConcurrentHashMap<String, ICacheClient>();
 
-    private CacheCmpFactory() {
-        // do nothing
-    }
+	private CacheCmpFactory() {
+		// do nothing
+	}
 
-    public static ICacheClient getClient(Properties config) {
-        GenericObjectPoolConfig genericObjectPoolConfig = new GenericObjectPoolConfig();
-        genericObjectPoolConfig.setMaxTotal(Integer.parseInt(config.getProperty("mcs.maxtotal", "500")));
-        genericObjectPoolConfig.setMaxIdle(Integer.parseInt(config.getProperty("mcs.maxIdle", "10")));
-        genericObjectPoolConfig.setMinIdle(Integer.parseInt(config.getProperty("mcs.minIdle", "5")));
-        genericObjectPoolConfig.setTestOnBorrow(Boolean.parseBoolean(config.getProperty("mcs.testOnBorrow", "true")));
+	public static ICacheClient getClient(Properties config) {
+		GenericObjectPoolConfig genericObjectPoolConfig = new GenericObjectPoolConfig();
+		genericObjectPoolConfig.setMaxTotal(Integer.parseInt(config.getProperty("mcs.maxtotal", "500")));
+		genericObjectPoolConfig.setMaxIdle(Integer.parseInt(config.getProperty("mcs.maxIdle", "10")));
+		genericObjectPoolConfig.setMinIdle(Integer.parseInt(config.getProperty("mcs.minIdle", "5")));
+		genericObjectPoolConfig.setTestOnBorrow(Boolean.parseBoolean(config.getProperty("mcs.testOnBorrow", "true")));
 
-        String host = config.getProperty("mcs.host", "127.0.0.1:6379");
+		String host = config.getProperty("mcs.host", "127.0.0.1:6379");
 
-        String[] hostArray = host.split(";");
-        ICacheClient cacheClient = null;
-        if (hostArray.length > 1) {
-            cacheClient = new CacheClusterClient(genericObjectPoolConfig, hostArray);
-        } else {
-            cacheClient = new CacheClient(genericObjectPoolConfig, host);
-        }
-        cacheClients.put(host, cacheClient);
-        return cacheClient;
-    }
+		String[] hostArray = host.split(";");
+		ICacheClient cacheClient = null;
+		if (null != cacheClients.get(host)) {
+			return cacheClients.get(host);
+		}
+		if (hostArray.length > 1) {
+			cacheClient = new CacheClusterClient(genericObjectPoolConfig, hostArray);
+		} else {
+			cacheClient = new CacheClient(genericObjectPoolConfig, host);
+		}
+		cacheClients.put(host, cacheClient);
+		return cacheClient;
+	}
 
-    public static ICacheClient getClient() throws IOException {
-        Properties config = new Properties();
-        config.load(CacheCmpFactory.class.getResourceAsStream("/redis.conf"));
-        return getClient(config);
-    }
+	public static ICacheClient getClient() throws IOException {
+		Properties config = new Properties();
+		config.load(CacheCmpFactory.class.getResourceAsStream("/redis.conf"));
+		return getClient(config);
+	}
 
-    public static ICacheClient getClient(InputStream inputStream) throws IOException {
-        Properties config = new Properties();
-        config.load(inputStream);
-        return getClient(config);
-    }
+	public static ICacheClient getClient(InputStream inputStream) throws IOException {
+		Properties config = new Properties();
+		config.load(inputStream);
+		return getClient(config);
+	}
 }
